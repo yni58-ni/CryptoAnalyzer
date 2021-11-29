e1637c5b0c97695d7e6dc98df4fc23e2435b69a1
Yeonsil Choi <ychoi348@uwo.ca>
2021-11-29 00:16:07 -0500

    created date.java

---
+++ cryptoAnalyzer/src/main/java/Date.java
@@ -0,0 +1,40 @@
+public class Date {
+
+	private int day;
+	private int month;
+	private int year;
+	
+	/***
+	 * constructor
+	 */
+	public Date(int d, int m, int y) {
+		this.day = d;
+		this.month = m;
+		this.year = y;
+		
+	}
+	
+	public int getDay() {
+		return this.day;
+	}
+	
+	public void setDay(int newDay) {
+		this.day = newDay;
+	}
+	
+	public int getMon() {
+		return this.month;
+	}
+	
+	public void setMon(int newMon) {
+		this.month = newMon;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public void setYear(int newYear) {
+		this.year = newYear;
+	}
+}
